aa7f3d6a715edaf4da3936392a2a6e101497648c
Changgeng Li <li.changgeng@gmail.com>
2015-02-08 08:59:15 -0800

    fix swap cell and refactor

--- src-cljs/minesweeper/core.cljs
+++ src-cljs/minesweeper/core.cljs
@@ -6,7 +6,7 @@
 (enable-console-print!)
 
 (def difficulties 
-  {:beginner      [8 8 10]
+  {:beginner      [5 4 6]
    :intermediate  [16 16 40]
    :expert        [30 16 99]})
 
@@ -16,7 +16,7 @@
 (def time-tick (r/atom 0))
 
 
-(defn set-tick[]
+(defn set-tick[] 
   (reset! time-tick (.getTime (js/Date.))))
 
 ;; game status
@@ -25,7 +25,7 @@
    {
   ;  :board []
   ;  :neighbor []
-  ;  :state [] ;; :hide, :open, :mark, :question
+  ;  :states [] ;; :hide, :open, :flag, :question
 
      :first true
      :start 0
@@ -62,20 +62,28 @@
        (map vec)
        (vec)))
 
-(defn neighbor-xy [x y]
+(defn surround [x y]
   ;;return all the neighbor's index but doesn't include itself
   ;;may be out of bound
   (for [a (list (dec x)  x (inc x)) 
         b (list (dec y) y (inc y)) :when (not (and (= x a) (= y b)))] [a b]))
    
 
-(defn count-neighbor [xx yy board ]
+(defn count-single-surround [board x y]
+;;  (println "counting cell " x y "for board " board)
+  (if (zero? (get-in board [x y]))
+        (->> 
+          (surround x y)
+          (map #(get-in board % 0))
+          (apply +))
+        \X ))
+
+;;count the whole board
+(defn count-surround [xx yy board ]
+  (println "counting " xx yy board)
   (->> 
-    (for [x (range xx) y (range yy)] 
-      (->> 
-        (neighbor-xy x y)
-        (map #(get-in board % 0))
-        (apply +)))
+    (for [x (range yy) y (range xx)] 
+      (count-single-surround board x y))
     (partition xx)
     (map vec)
     (vec) ))
@@ -85,14 +93,14 @@
   {:x x
    :y y
    :board board
-   :counts (count-neighbor x y  board) ;mines of the neighbors
-   :state (vec (for [a (range y)]
+   :counts (count-surround x y  board) ;mines of the neighbors
+   :states (vec (for [a (range y)]
                  (vec (for [b (range x)] 
                      :hide ))))
-   :mines mines
    :end false
    :first true
    :start 0 ;;timestamp
+   :remains mines
    }))
 
 (defn all-open? [game]
@@ -100,7 +108,7 @@
           (apply concat)
           (filter #(not= 1 %))
           (count))
-     (->> (game :state)
+     (->> (game :states)
           (apply concat)
           (filter #(= :open %))
           (count))))
@@ -115,15 +123,15 @@
   (-> game
       (assoc-in [:end] :failed)))
 
-(defn open-neighbors [game col mark-to-open]
+(defn open-surround [game col mark-to-open]
   (if (seq col)
     (let [ [x y]    (peek col)  
          can-open #{:hide :question}
-         to-open  (->> (neighbor-xy x y)
-                       (filter #(can-open (get-in game (into [:state] %)) ))
+         to-open  (->> (surround x y)
+                       (filter #(can-open (get-in game (into [:states] %)) ))
                        (filter (complement mark-to-open))) ] 
       (recur 
-        (reduce #(assoc-in % (into [:state] %2) :open) game to-open)
+        (reduce #(assoc-in % (into [:states] %2) :open) game to-open)
         (into (pop col) (filter #(zero? (get-in game (into [:counts] %) 99)) to-open))
         (into mark-to-open to-open)))
     ;; end of traverse
@@ -132,19 +140,19 @@
 ;; make the first click lucky
 ;; and swap
 (defn swap-cell [game c1 c2]
-  (println "swapping" c1 c2)
+;  (println "swapping" c1 c2)
   (let [c1v      (get-in game (into [:board ] c1) )
         c2v      (get-in game (into [:board ] c2) )
         to-count (-> #{c1 c2} 
-                     (into (apply neighbor-xy c1))
-                     (into (apply neighbor-xy c2)))
+                     (into (apply surround c1))
+                     (into (apply surround c2)))
         ;swap the bomb
         game     (-> game
                      (assoc-in (into [:board] c1) c2v)
                      (assoc-in (into [:board] c2) c1v))]
     (assoc game :counts 
            (reduce #(if (get-in % %2)  ;could be outofbound
-                      (assoc-in % %2 (apply count-neighbor %2))
+                      (assoc-in % %2 (apply count-single-surround (game :board) %2))
                       %)
                       (game :counts) to-count))))
         
@@ -173,27 +181,27 @@
                 (assoc :start (.getTime (js/Date.)) )
                 (assoc :first false))
                game) ]
-    (condp = (get-in game [:state x y])
-      :mark game
+    (condp = (get-in game [:states x y])
+      :flag game
       :open game 
-      (let [game (assoc-in game [:state x y] :open)]
+      (let [game (assoc-in game [:states x y] :open)]
            (cond 
              (= (get-in game [:board x y ]) 1) (fail-game game)  ;bomb
              (all-open? game) (complete-game game)               ;all openned
-             (zero? (get-in game [:counts x y])) (open-neighbors game [[x y]] #{[x y]}) ;open all neighors
+             (zero? (get-in game [:counts x y])) (open-surround game [[x y]] #{[x y]}) ;open all neighors
              :else game)))))
 
 
 (defn mark-game [game x y]
-  (let [state (condp = (get-in game [:state x y])   
-               :hide :mark
+  (let [state (condp = (get-in game [:states x y])   
+               :hide :flag
                :open :open
-               :mark :question
+               :flag :question
                :question :hide)
-        game (assoc-in game [:state x y ] state) ]
+        game (assoc-in game [:states x y ] state) ]
     (condp = state
-      :mark (update-in game [:mines] dec)
-      :question (update-in game [:mines] inc)
+      :flag (update-in game [:remains] dec)
+      :question (update-in game [:remains] inc)
       game)))
 
 ;;;
@@ -202,12 +210,12 @@
 ;;; if counts == marked neighbors
 ;;;
 (defn explore-game [game x y]
-  (if (not= (get-in game [:state x y]) :open)
+  (if (not= (get-in game [:states x y]) :open)
     game
-    (let [to-open (->> (neighbor-xy x y)
-                       (filter #(#{:hide :question} (get-in game (into [:state] %) :outbound))))
-          marked  (->> (neighbor-xy x y)
-                       (filter #(= :mark (get-in game (into [:state] %) :outbounded)))
+    (let [to-open (->> (surround x y)
+                       (filter #(#{:hide :question} (get-in game (into [:states] %) :outbound))))
+          marked  (->> (surround x y)
+                       (filter #(= :flag (get-in game (into [:states] %) :outbounded)))
                        (count))
           counts  (get-in game [:counts x y])]
       (cond
@@ -215,24 +223,27 @@
         (not= marked counts) game
         :else (reduce #(apply click-game % %2) game to-open)))))
 
-(defn new-game! [difficulty]
-  (stop-sound)
-  (reset! time-tick 0)
-  (reset! game-state (apply init-game (difficulties difficulty))))
 
 (defn print-game [game]
+  (println "=*- " (name (or (game :end) :enjoy)) "-*=")
   (println "=================")
-  (when (game :end)
-  (println "*    " (name (game :end)) "     *")
-  (println "================="))
   (doall (map #(println (apply str (map str %))) (game :board)))
   (println "=================")
-  (doall (map #(println (apply str (map (comp str first name ) %))) (game :state)))
+  (doall (map #(println (apply str (map (comp str first name ) %))) (game :states)))
   (println "=================")
   (doall (map #(println (apply str (map str %))) (game :counts)))
   (println "================="))
 
+(defn new-game! [difficulty]
+  (stop-sound)
+  (reset! time-tick 0)
+  (reset! game-state (apply init-game (difficulties difficulty)))
+  (print-game @game-state)
+  )
+
+
 (defn click! [x y]
+;;  (println @game-state)
   (when-not (@game-state :end)
     (swap! game-state click-game x y)))
 
@@ -245,63 +256,66 @@
     (swap! game-state explore-game x y)))
 ;;
 ;; :open -> dblclick
-;; :mark -> rightclick
+;; :flag -> rightclick
 ;; :question -> click, rightclick
 ;; :hide -> click, rightclick
-(defn cell [x y ]
-  (let [game  @game-state
-        state (get-in game [:state x y] )
-        c     (get-in game [:counts x y])
-        value (get-in game [:board x y])]
+(defn cell [x y mine state surround]
        [:span.cell
           {
           :on-click #(click! x y)
           :on-context-menu  #(do (mark! x y ) false)
           :on-double-click #(explore! x y)
           :class (cond 
-                   (= state :open) (if (zero? value)
-                                     (str "open_" c)
+                   (= state :open) (if (zero? mine)
+                                     (str "open_" surround)
                                      "cell_mine")
                    :else (str "cell_" (name state)))}
          (cond 
                    (= state :open) (cond
-                                     (= 1 value) "X"
-                                     (> c 0) (str  c)
+                                     (= 1 mine) "X"
+                                     (> surround 0) (str  surround)
                                      :else "0")
                    :else (first (name state)))
+          ])
 
-          ]))
-(defn game-row [game y]
-  (into [:div.game-row ] (for [x (range (game :x))] (cell x y ))))
+(defn game-row [x mines states surrounds]
+;;  (println "rending " x mines states surrounds)
+  (into [:div.game-row ] 
+        (map cell (repeat x) (range) mines states surrounds)));
+
+;        (for [y (range (game :y))] (cell x y ))))
 
 (defn format [n]
   (let [s (str n)]
     (str (apply str (repeat (- 3 (count s)) 0)) s)))
+
 (defn timer-component[]
 ;;  (println "trigger timer and rendering timer component ")
   (js/setTimeout update-timer 200)
-  [:input#display {:type      "text" 
+  [:input.display {:type      "text" 
                    :read-only true
                    :value     (format (min 999 (quot (- @time-tick (@game-state :start)) 1000)))} ])
 
 
 ;; UI components
 (defn game-table []
+  (println "rendering " @game-state)
   (let [game @game-state] 
     (into [:div.board-panel 
            [:div.control-row.row  
-            [:input#display {:type "text" 
+            [:input.display {:type "text" 
                              :read-only true
-                             :value (format (game :mines))} ]
+                             :value (format (game :remains))} ]
             [:button#control {:on-click #(new-game! :beginner)} "\u263A" ] 
             (timer-component)
             ]]
-
-        (for [y (range (game :y))]
-          (game-row game y)))))
+        (map game-row (range) (game :board) (game :states) (game :counts)))))
+ ;       (for [x (range (game :x))]
+ ;         (game-row game x)))))
 
 ;; Render the root component
 (defn start []
+  (new-game! :beginner)
   (r/render-component 
    [game-table]
    (.getElementById js/document "root")))
